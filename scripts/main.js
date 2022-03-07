81a4f6e6bc86e0ec028f772f7650fc8b36516a7e
ActuallyAJL <zero3260@hotmail.com>
2022-03-07 11:42:03 -0600

    project done

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,42 @@
+const buyClay = () => {
+    let potObject = {};
+    return potObject;
+}
 
+const makePottery = (obj) => {
+    obj.shape = "Bowl";
+    return obj;
+}
+
+const bisqueFire = (obj) => {
+    obj.readyForGlazing = true;
+    return obj;
+}
+
+const glazePottery = (obj) => {
+    if (obj.readyForGlazing) {
+        obj.glazing = "Midnight Blue";
+    }
+    else {
+        console.log("Make sure you bisque fire the pottery before you glaze it.");
+    }
+    return obj;
+}
+
+const finalFiring = (obj , temp) => {
+    if (temp > 1200) {
+        obj.cracked = true;
+    }   
+    else {
+        obj.cracked = false;
+    }
+    return obj;
+}
+
+let myPot = {};
+buyClay(myPot);
+makePottery(myPot);
+bisqueFire(myPot);
+glazePottery(myPot);
+finalFiring(myPot);
+console.log(myPot);
